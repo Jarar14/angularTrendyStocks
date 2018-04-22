dd0587b16724f3eefb87686b74f0fed14b96190f
benjaminwyss <wyssbenjamin17@gmail.com>
2018-04-21 23:35:19 -0500

    added angular app

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { StockService } from './stock.service';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule
+  ],
+  providers: [StockService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
